83708386d0b151b45de10eabb5644af8f98dbeca
Wayne Andrew <wayne.andrew@gmail.com>
2020-03-14 14:27:10 -0600

    I know this seems silly, but I want to quickly do all the easy ones to demonstrate I know at least something about the Go language.

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -7,9 +7,5 @@ package greeting
 
 // HelloWorld should have a comment documenting it.
 func HelloWorld() string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	return "Hello, World!"
 }
